ec3a8782a13eb7c5c70848f7db2c47d569882237
Tobi DEGNON <tobidegnon@proton.me>
2024-09-08 22:53:34 +0100

    basic templates structure

--- src/falco/bootstrap/apps.py
+++ src/falco/bootstrap/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FalcoFaviconsConfig(AppConfig):
+class FalcoBootstrapConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "falco.favicons"
+    name = "falco.bootstrap"
--- src/falco/tailwind/apps.py
+++ src/falco/tailwind/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FalcoFaviconsConfig(AppConfig):
+class FalcoTailwindConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "falco.favicons"
+    name = "falco.tailwind"
